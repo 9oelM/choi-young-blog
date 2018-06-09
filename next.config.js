a2b8fdc1dfec42f6258304474471de7ada79ea91
9oelM <hj923@hotmail.com>
2018-06-09 03:49:41 +0000

    add eslint settings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,26 @@
-const withCss = require('@zeit/next-css');
+const withCss = require('@zeit/next-css'); //  just in case if you use css
 const withSass = require('@zeit/next-sass')
 const webpack = require('webpack');
+const {assocPath} = require('ramda');
 
 module.exports = withSass(withCss({ 
-    
-  }));
+    webpack: (config, { dev }) => {
+    const eslintRule = {
+      test: /\.js$/,
+      enforce: 'pre',
+      exclude: /node_modules/,
+      loader: 'eslint-loader',
+      options: {
+        // Emit errors as warnings for dev to not break webpack build.
+        // Eslint errors are shown in console for dev, yay :-)
+        emitWarning: dev,
+      },
+    };
+
+    const rules = [].concat(eslintRule, config.module.rules);
+    return assocPath(['module', 'rules'], rules, config);
+    }
+}));
   /* extra optional config */ 
 
 /*
@@ -44,4 +60,22 @@ config.module.rules.push(
     ]
   }
 );
+*/
+
+/*
+module.exports = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.module.rules.push({
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          // eslint options (if necessary)
+        }
+      })
+    }
+    return config
+  }
+}
 */

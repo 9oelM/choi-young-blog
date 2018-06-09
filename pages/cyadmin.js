8b0f572a4afb16e7596fcf6c362361f63c064b68
9oelM <hj923@hotmail.com>
2018-06-09 10:26:38 +0000

    set initial next auth config

--- pages/cyadmin.js
+++ pages/cyadmin.js
@@ -1,7 +1,16 @@
 import React from "react";
 import LoginForm from 'grommet/components/LoginForm';
+import Layout from "../components/layout";
+import Card from "grommet/components/Card";
+import Box from "grommet/components/Box";
+import Grommet from "grommet";
 
-export default () => (<LoginForm onSubmit={f=>f}
-  title='Sample Title'
-  rememberMe={false}
-  usernameType='text' />);
+export default () => (
+    <Layout>
+    <LoginForm onSubmit={f=>f}
+  title='Admin login'
+  rememberMe={true}
+  secondaryText='Welcome, 형'
+  usernameType='text' />
+  </Layout>
+ );

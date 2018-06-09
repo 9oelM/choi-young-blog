9370e9f72faa91d8f72bd1b60a7b5432a11ae920
9oelM <hj923@hotmail.com>
2018-06-09 09:22:32 +0000

    move grommet.app to layout.js

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import Layout from "../components/layout";
 import Card from "grommet/components/Card";
-import Grommet from "grommet";
-import SlideMenu from "../components/slideMenu";
-import MenuIcon from "grommet/components/icons/base/Menu";
 import "../styles/master.sass";
 
 export default () => (
-	<Grommet.App centered = {false}>
-		<SlideMenu title = {"Choi Young"}/>
-		<Layout >
+
+	<Layout >
 			<Card thumbnail='https://images.pexels.com/photos/374685/pexels-photo-374685.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
 				label='Sample Label'
 				heading='Sample Heading'
@@ -23,5 +19,5 @@ export default () => (
 				heading='Sample Heading'
 				description='Sample description providing more details.' />
 		</Layout>
-	</Grommet.App>
+
 );

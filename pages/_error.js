f9537a2d2482539ce062d9516b699dfab917e1c6
9oelM <hj923@hotmail.com>
2018-06-10 06:34:50 +0000

    fix styling with eslint

--- pages/_error.js
+++ pages/_error.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import Error from 'next/error'
-import fetch from 'isomorphic-unfetch'
+import React from "react";
+import Error from "next/error";
+import fetch from "isomorphic-unfetch";
 
 export default class Page extends React.Component {
-  static async getInitialProps() {
-    const res = await fetch('https://api.github.com/repos/zeit/next.js')
-    const statusCode = res.statusCode > 200 ? res.statusCode : false
-    const json = await res.json()
+	static async getInitialProps() {
+		const res = await fetch("https://api.github.com/repos/zeit/next.js");
+		const statusCode = res.statusCode > 200 ? res.statusCode : false;
+		const json = await res.json();
 
-    return { statusCode, stars: json.stargazers_count }
-  }
+		return { statusCode, stars: json.stargazers_count };
+	}
 
-  render() {
-    if (this.props.statusCode) {
-      return <Error statusCode={this.props.statusCode} />
-    }
+	render() {
+		if (this.props.statusCode) {
+			return <Error statusCode={this.props.statusCode} />;
+		}
 
-    return (
-      <div>
+		return (
+			<div>
         Next stars: {this.props.stars}
-      </div>
-    )
-  }
+			</div>
+		);
+	}
 }
